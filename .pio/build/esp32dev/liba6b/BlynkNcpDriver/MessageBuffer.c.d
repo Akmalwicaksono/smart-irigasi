1ea31248bc14ce4068d34d821e17afb06f3b6fff
Akmal Wicaksono <akmalwicaksono@github.com>
2026-07-27 17:40:49 +0700

    Remove build artifacts from git tracking

--- .pio/build/esp32dev/liba6b/BlynkNcpDriver/MessageBuffer.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/liba6b/BlynkNcpDriver/MessageBuffer.c.o: \
- .pio/libdeps/esp32dev/BlynkNcpDriver/src/MessageBuffer.c \
- .pio/libdeps/esp32dev/BlynkNcpDriver/src/MessageBuffer.h
